9c76b9a429d575ec0393900708871780351a20d5
Bernard Olaires <bolairesjr@Bernards-MacBook-Air.local>
2022-06-20 00:48:19 -0700

    JS Notes 6/20/22

---
+++ Coding Notes/JS Notes/script.js
@@ -0,0 +1,14 @@
+//JS example of button click using periodic elements
+function displayName(elementName) {
+    console.log(elementName);
+}
+
+//JS Example of (this)
+function example(element) {
+    console.log("element clicked", element);
+}
+
+//JS example of (this) and .innertext property
+function turnOff(element) {
+    element.innerText = "Off";
+}
