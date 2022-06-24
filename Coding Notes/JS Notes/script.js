6286dc63ed85e31a5d272e102f896eef1df58b1a
Bernard C Olaires Jr <Bolairesjr@gmail.com>
2022-06-24 10:43:15 -0700

    6/24/22 notes

--- Coding Notes/JS Notes/script.js
+++ Coding Notes/JS Notes/script.js
@@ -1,27 +1,25 @@
-//example of button click and showing to console
+//example of button click and showing to console (23-34)
 function displayName(elementName) {
     console.log(elementName);
 }
 
-//Example of (this)------------------------------------------
-
-// console log "click me"
+// console log "click me" (39-41)
 function example(element) {
     console.log("element clicked", element);
 }
 
-//changing button name when clicked 
+//changing button name when clicked (46-48)
 function turnOff(element) {
     element.innerText = "Off";
 }
 
-//hiding a button 
+//hiding a button (53)
 
 function hide(element) {
     element.remove();
 }
 
-//hover
+//hover (58-63)
 
 function addShadow(element) {
     element.classList.add("shadow");
@@ -32,7 +30,7 @@ function removeShadow(element) {
 }
 
 
-//hoover continued
+//hoover continued (66-67)
 
 function over(element) {
     alert("mouseover");
@@ -41,3 +39,41 @@ function over(element) {
 function out(element) {
     alert("mouseout")
 }
+
+// QuerySelector (70-72)
+
+var count = 1;
+var countElement = document.querySelector("#count");
+
+console.log(countElement);
+
+function add1() {
+    count++;
+    countElement.innerText = "the count is " + count;
+    console.log(count);
+}
+
+function subtract1() {
+    count--;
+    countElement.innerText = "the count is " + count;
+    console.log(count);
+}
+
+//changing html and css (75-)
+
+var animalImg = document.querySelector("#animal");
+
+console.log(animalImg);
+
+function pickCat(element) {
+    element.style.backgroundColor = "goldenrod" ;
+    animalImg.src = "./img/cat.jpg";
+    console.log(animalImg.src);
+}
+
+function pickDog(element) {
+    element.classList.add("btn");
+    animalImg.src = "./img/dog.jpg"
+    console.log(animalImg.src);
+}
+
