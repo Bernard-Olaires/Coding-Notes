1f75490d6f4dc9c2961e2b826e75c5ef449013eb
Bernard Olaires <bolairesjr@gmail.com>
2022-07-21 17:54:37 -0600

    notes

--- Coding Notes/PYTHON_Notes/notes.py
+++ Coding Notes/PYTHON_Notes/notes.py
@@ -819,3 +819,69 @@ print(resume_data["skills"][1])
 print(users[2]["first"])
 
 
+# ---------------------------------------------------------------------------------------------
+
+
+# -- PYTHON OOP -- 
+
+
+## -- Class -- 
+
+# - Syntax - 
+class User:
+    pass # we'll fill this in shortly
+
+
+#A constructor is a function that contains instructions for making a new instance of a class
+
+# attributes 
+
+# declare a class and give it name Shoe
+class Shoe:		
+    def __init__(self):
+        self.brand = "Vans"
+        self.type = "Classic Sk8-Hi"
+        self.price = 69.99
+        self.in_stock = True
+
+skater_shoe = Shoe()
+dress_shoe = Shoe()
+# Accessing the instance's attributes
+print(skater_shoe.type) # Classic Sk8-Hi
+print(dress_shoe.type)	# Classic Sk8-Hi
+
+
+class Shoe:
+    # now our method has 4 parameters (including self)!
+    def __init__(self, brand, shoe_type, price):
+    # we assign them accordingly
+        self.brand = brand
+        self.type = shoe_type
+        self.price = price
+        # the status is set to True by default
+        self.in_stock = True
+skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
+dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
+print(skater_shoe.type)	# output: Low-top Trainers
+print(dress_shoe.type)	# output: Ballet Flats
+
+
+
+class User:		# here's what we have so far
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    # adding the greeting method
+    def greeting(self):
+        print(f"Hello, my name is {self.name}")
+
+
+adrien = User("Adrien", "adion@codingdojo.com")
+cool_person = User("Sadie", "sflick@codingdojo.com")
+    
+adrien.greeting()
+# prints Hello, my name is Adrien
+    
+cool_person.greeting()
+# prints Hello, my name is Sadie
+
